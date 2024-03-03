ff29dc0c205e62f665da7634e14d6ca795e14b0b
Hafeez-nabeel <hafeezbulushi@gmail.com>
2024-03-03 12:12:06 +0500

    changed home page

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/person", personRoute)
 app.use("/menuitem", menuItemRout)
-
 const PORT = process.env.PORT || 3000
+app.get("/", (req, res)=>{
+res.send("Welcome to world's best hotel")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
